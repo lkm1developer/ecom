67da42781aea86e0eb7176b410273f3fb23b20f5
lkm1developer <lkm1developer@gmail.com>
2023-03-22 17:19:31 +0530

    homepage

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+	const navigate =useNavigate()
+
+	const handleClick =()=>{
+		navigate(linkUrl)
+	}
+
+	return(
+	<div className={`${size} menu-item`} onClick={handleClick}>
+		<div className="background-image"  style={{
+			backgroundImage: `url(${imageUrl})`
+		}} ></div>
+		<div className="content">
+			<h1 className="title">{title.toUpperCase()}</h1>
+			<span className="subtitle">SHOP NOW</span>
+		</div>
+	</div>
+)}
+
+export default MenuItem;
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/Directory';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+	<div className="homepage">
+		<Directory></Directory>
+	</div>
+)
+
+export default HomePage;
