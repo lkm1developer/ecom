49e2ec054c39cdb93625c53f94e522456c15176b
lkm1developer <lkm1developer@gmail.com>
2023-03-22 17:34:27 +0530

    homepage test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,getByTestId } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Homepage categories Test', () => {
+ const c= render(<App />);
+  const hats = screen.getByText(/HATS/i);
+  expect(hats).toBeInTheDocument();
+  const jackets = screen.getByText(/JACKETS/i);
+  expect(jackets).toBeInTheDocument();
+  const sneakers = screen.getByText(/SNEAKERS/i);
+  expect(sneakers).toBeInTheDocument();
+  const womens = screen.getByText(/WOMENS/i);
+  expect(womens).toBeInTheDocument();
 });
