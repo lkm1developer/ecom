065e89be842981ca309eff396cfd45fe8786cf48
lkm1developer <lkm1developer@gmail.com>
2023-03-28 09:33:52 +0530

    bug fix 2

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { toggleCartHidden } from './../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
-import { ReactComponent as ShoppingIcon} from './../../assets/shopping-bag.svg';
 import { useDispatch, useSelector } from 'react-redux';
 
 const CartIcon = () => {
@@ -16,9 +15,10 @@ const CartIcon = () => {
 
   return(
   <div className="cart-icon" onClick={toggleCartHiddenHandler} data-testid="cart-icon">
-    <ShoppingIcon className="shopping-icon" />
+    {/* <ShoppingIcon className="shopping-icon" /> */}
     <span className="item-count" data-testid="item-count">{ itemCount }</span>
   </div>
 )}
 
 export default CartIcon;
+
