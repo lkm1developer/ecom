82d4f0bbffbe7bed1bf957759246bbe46f88d51b
lkm1developer <lkm1developer@gmail.com>
2023-05-08 16:40:05 +0530

    t3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from './components/Header';
-// import Homepage from './pages/Homepage';
+import Homepage from './pages/Homepage';
 
 const App = () => {
 
@@ -11,7 +11,9 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Header />
-       
+        <Routes>
+          <Route path="/" index element={<Homepage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
